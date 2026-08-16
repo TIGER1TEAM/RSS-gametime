97b9562a78b6316879ab1f7ac5a5f174c0555325
TIGER TEAM <146400879+TIGER1TEAM@users.noreply.github.com>
2026-08-16 13:16:00 -0500

    Change timezone from US/Central to America/Chicago

--- update_scores.py
+++ update_scores.py
@@ -5,7 +5,7 @@ from zoneinfo import ZoneInfo # Standard in Python 3.9+
 
 def update_scores():
     # Set to US/Central time using built-in library
-    tz = ZoneInfo('US/Central')
+    tz = ZoneInfo('America/Chicago')
     today = datetime.now(tz).strftime('%Y%m%d')
     
     # ESPN API (Groups 80 = FBS Division 1)
